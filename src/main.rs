c61b0d0f1707b3c9e916c5ae0f5a1d1f8f9238ba
Max Jacobson <max@hardscrabble.net>
2016-04-10 00:50:13 +0530

    Ignore "hidden" files and folders (start with .)
    Also improve the output a bit...

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,17 @@ fn list_dir(path: &Path, nesting: u32) {
     let entries = read_dir(path).unwrap();
     let mut padding = "".to_owned();
     for _ in 0..nesting {
-        padding.push('\t');
+        padding.push(' ');
+        padding.push(' ');
     }
     for entry in entries {
         let entry = entry.unwrap();
         let entry_type = entry.file_type().unwrap();
         let entry_path = entry.path();
-        let name_of_path = entry_path.as_path().to_str().unwrap();
+        let name_of_path = entry_path.as_path().file_name().unwrap().to_str().unwrap();
+        if name_of_path.chars().nth(0).unwrap() == '.' {
+            continue;
+        }
         if entry_type.is_dir() {
             println!("{}{}", padding, name_of_path);
             list_dir(entry.path().as_path(), nesting + 1);
